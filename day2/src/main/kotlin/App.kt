d953b259bddc63b72fe19e8a10ec44eed19843b8
Nicholas Lediet <nicklediet@gmail.com>
2025-12-10 12:55:15 -0500

    Initialize project

---
+++ day2/src/main/kotlin/App.kt
@@ -0,0 +1,9 @@
+package com.nle.aoc.day2
+
+import
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+   println("Hello from day2")
+}
