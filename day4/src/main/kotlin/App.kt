0bc8e4ce02aa881dd4d770fc6b31607e4b9f6deb
Nicholas Lediet <nicklediet@gmail.com>
2025-12-10 19:37:34 -0500

    Add in progress day4

--- day4/src/main/kotlin/App.kt
+++ day4/src/main/kotlin/App.kt
@@ -4,24 +4,56 @@ import com.nle.aoc.utils.Solution
 import com.nle.aoc.utils.readResourceAsLines
 import java.lang.Math
 
-class Day4Solution() : Solution<List<String>, Long> {
+class Day4Solution(val paperChar: Char = '@') : Solution<List<String>, Long> {
     init {
         println("Initialized Day 4 solution")
     }
-  
+
+    fun checkNumberOfAdjacentPaperChars(input: List<String>, rowIndex: Int, colIndex: Int): Int {
+        var ans = 0
+        if(rowIndex > 0 && input[rowIndex - 1][colIndex] == paperChar) ans += 1
+        if(rowIndex < input.size - 1 && input[rowIndex + 1][colIndex] == paperChar) ans += 1
+        if(colIndex > 0 && input[rowIndex][colIndex - 1] == paperChar) ans += 1
+        if(colIndex < input[rowIndex].length - 1 && input[rowIndex][colIndex + 1] == paperChar) ans += 1
+		// Diagonals
+		if(rowIndex > 0 && colIndex > 0 && input[rowIndex - 1][colIndex - 1] == paperChar) ans += 1
+		if(rowIndex > 0 && colIndex < input[rowIndex].length - 1 && input[rowIndex - 1][colIndex + 1] == paperChar) ans += 1
+		if(rowIndex < input.size - 1 && colIndex > 0 && input[rowIndex + 1][colIndex - 1] == paperChar) ans += 1
+		if(rowIndex < input.size - 1 && colIndex < input[rowIndex].length - 1 && input[rowIndex + 1][colIndex + 1] == paperChar) ans += 1
+        return ans
+    }
     override fun part1(input: List<String>): Long {
-        return input.size.toLong()
+        var ans = 0L
+        for((rowIndex, row) in input.withIndex()) {
+            for((colIndex, col) in row.withIndex()) {
+                if(col != paperChar) continue
+				if(checkNumberOfAdjacentPaperChars(input, rowIndex, colIndex) < 4) ans += 1
+            }
+        }
+        return ans
     }
 
     override fun part2(input: List<String>): Long {
-        return input.size.toLong()
+        var ans = 0L
+        // for((rowIndex, row) in input.withIndex()) {
+        //     for((colIndex, col) in row.withIndex()) {
+        //         if(col != paperChar) continue
+		// 		if(checkNumberOfAdjacentPaperChars(input, rowIndex, colIndex) < 4) {
+        //             ans += 1
+        //             var lineString = row;
+        //             lineString[colIndex] = 'x';
+        //             input[rowIndex] = lineString;
+        //         }
+        //     }
+        // }
+        return ans
     }
 }
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    var fileContents = readResourceAsLines("/example-input.txt", ",")
+    var fileContents = readResourceAsLines("/input.txt")
     var solution = Day4Solution()
     fileContents?.let {
         println("Part 1: ${solution.part1(fileContents)}")
--- day4/src/test/kotlin/AppTest.kt
+++ day4/src/test/kotlin/AppTest.kt
@@ -1,12 +1,26 @@
 package com.nle.aoc.day4
 
 import com.nle.aoc.utils.readResourceAsLines
+import com.nle.aoc.day4.Day4Solution
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 internal class AppTest {
+    // part 1 example input should return 13
     @Test
-    fun exampleTestShouldPass() {
-        assertEquals(true, true)
+    fun testPart1WithExampleInputShouldReturn13AsTheAnswer() {
+        val input = readResourceAsLines("/example-input.txt")
+        val solution = Day4Solution()
+        input?.let {
+            assertEquals(13, solution.part1(it))
+        } ?: throw Exception("No input found")
+    }
+    @Test
+    fun testPart1WithRealInputShouldReturn1416AsTheAnswer() {
+        val input = readResourceAsLines("/input.txt")
+        val solution = Day4Solution()
+        input?.let {
+            assertEquals(1416, solution.part1(it))
+        } ?: throw Exception("No input found")
     }
 }
